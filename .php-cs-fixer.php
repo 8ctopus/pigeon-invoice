9203538518e13881d75418e765633fa2c786c80e
8ctopus <hello@octopuslabs.io>
2023-01-27 16:12:17 +0400

    updated: php cs fixer rules

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -7,11 +7,24 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PhpCsFixer' => true,
         'blank_line_before_statement' => [
-            'statements' => ['case', 'default'],
+            'statements' => [
+                'case',
+                'default',
+            ],
         ],
-        'concat_space' => ['spacing' => 'one'],
-        'echo_tag_syntax' => ['format' => 'short'],
+        'concat_space' => [
+            'spacing' => 'one'
+        ],
+        'echo_tag_syntax' => [
+            'format' => 'short'
+        ],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => false,
+            'import_functions' => false,
+        ],
         'multiline_whitespace_before_semicolons' => false,
+        'no_empty_comment' => false,
         'no_superfluous_phpdoc_tags' => false,
         'no_useless_else' => false,
         'phpdoc_no_empty_return' => false,
@@ -19,7 +32,9 @@ return (new PhpCsFixer\Config())
         'phpdoc_trim' => true,
         'phpdoc_trim_consecutive_blank_line_separation' => true,
         'php_unit_method_casing' => false,
-        'return_type_declaration' => ['space_before' => 'one'],
+        'return_type_declaration' => [
+            'space_before' => 'one',
+        ],
         'single_line_comment_spacing' => false,
         'yoda_style' => false,
     ])
