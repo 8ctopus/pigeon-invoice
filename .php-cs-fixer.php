617869d08dde85996b23138cadff60121d0b3ef1
8ctopus <hello@octopuslabs.io>
2023-02-15 10:57:59 +0400

    added: phpstan and phpmd updated: composer scripts added: composer stability

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,9 +1,11 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
+    ->exclude('vendor/')
+//    ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
     ->in('.');
 
-return (new PhpCsFixer\Config())
+$fixer = (new PhpCsFixer\Config('', ''))
     ->setRules([
         '@PhpCsFixer' => true,
         'blank_line_before_statement' => [
@@ -13,10 +15,10 @@ return (new PhpCsFixer\Config())
             ],
         ],
         'concat_space' => [
-            'spacing' => 'one'
+            'spacing' => 'one',
         ],
         'echo_tag_syntax' => [
-            'format' => 'short'
+            'format' => 'short',
         ],
         'global_namespace_import' => [
             'import_classes' => true,
@@ -27,11 +29,13 @@ return (new PhpCsFixer\Config())
         'no_empty_comment' => false,
         'no_superfluous_phpdoc_tags' => false,
         'no_useless_else' => false,
+        'phpdoc_add_missing_param_annotation' => true,
         'phpdoc_no_empty_return' => false,
         'phpdoc_summary' => false,
         'phpdoc_trim' => true,
         'phpdoc_trim_consecutive_blank_line_separation' => true,
         'php_unit_method_casing' => false,
+        'psr_autoloading' => false,
         'return_type_declaration' => [
             'space_before' => 'one',
         ],
@@ -39,3 +43,19 @@ return (new PhpCsFixer\Config())
         'yoda_style' => false,
     ])
     ->setFinder($finder);
+
+if (isset($GLOBALS['argv']) && in_array('--allow-risky=yes', $GLOBALS['argv'], true)) {
+    echo 'Risky rules enabled' . PHP_EOL;
+
+    $fixer
+        ->setRules([
+            '@PhpCsFixer:risky' => true,
+            'native_function_invocation' => false,
+            'phpdoc_to_param_type' => true,
+            'phpdoc_to_property_type' => true,
+            'phpdoc_to_return_type' => true,
+            'void_return' => true,
+        ]);
+}
+
+return $fixer;
